839caa7fb6e52970108b1f4a80b592858944b42c
Thomas Frank <thomas.frank@esrlabs.com>
2021-10-19 08:41:07 +0200

    implemented optional.rs

--- src/elements/optional.rs
+++ src/elements/optional.rs
@@ -1,3 +1,49 @@
-struct Optional{
-    
+use crate::{ParseElement, ParseOk};
+
+struct Optional<T: ParseElement>{
+    elem: T
+}
+
+impl<T: ParseElement> Optional<T>{
+    fn new(elem: T) -> Optional<T>{
+        Optional{elem}
+    }
+}
+
+impl<T> ParseElement for Optional<T>
+where
+    T: ParseElement
+{
+    type ParseOut = Option<T::ParseOut>;
+
+    fn pars(&self, input: &str) -> crate::ParseResult<Self::ParseOut> {
+        if let Ok(ParseOk{bytes_parsed, result}) = self.elem.pars(input){
+            Ok(ParseOk{
+                bytes_parsed,
+                result: Some(result)
+            })
+        }else{
+            Ok(ParseOk{
+                bytes_parsed:0,
+                result: None
+            })
+        }
+    }
+}
+
+#[test]
+fn optional(){
+    let parser = Optional::new("Maybe");
+
+    let result = parser.pars("");
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert_eq!(*result, None);
+    assert_eq!(result.bytes_parsed, 0);
+
+    let result = parser.pars("Maybe");
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert_eq!(*result, Some("Maybe".into()));
+    assert_eq!(result.bytes_parsed, 5);
 }
