413dffa1b1d7bce77c382b0488e618ef70c37e99
Thomas Frank <thomas.frank@esrlabs.com>
2021-10-21 15:09:06 +0200

    changed brackets example to use + and |

--- examples/brackets.rs
+++ examples/brackets.rs
@@ -11,11 +11,8 @@ fn main() {
     let secret = Literal("secret").map(|s| Bracketed::Item(s));
 
     brackets.init(
-        Literal("(")
-            .tup(brackets.weak())
-            .tup(Literal(")"))
-            .map(|((_, inner), _)| Bracketed::Bracketed(Box::new(inner)))
-            .or(secret),
+        (Literal("(") + brackets.weak() + Literal(")")).map(|((_, inner), _)| Bracketed::Bracketed(Box::new(inner)))
+            | secret,
     );
 
     let to_parse ="((((((((((secret))))))))))";
